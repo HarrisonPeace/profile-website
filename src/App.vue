1bbee5d084c5dcc8d895b308cf1c1c7ee59fe78a
Harrison Peace <harrison.peace@revium.com.au>
2023-09-24 14:08:36 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div>
+      <HelloWorld />
+    </div>
+  </header>
+
+  <main></main>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="flex p-9 items-center justify-centers">
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </div>
+</template>
